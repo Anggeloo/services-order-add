d4db377430731e20f3ca6489b587c835239e8f9f
Anggelo <anstalyn1999@gmail.com>
2025-02-06 14:25:28 -0500

    Structured Folder Addition

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using services_order_add.Services;
+using services_order_add.Models;
+
+
+[ApiController]
+[Route("[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly OrderServices _orderService;
+    private readonly HttpClient _httpClient;
+
+    public OrdersController(OrderServices orderService, HttpClient httpClient)
+    {
+        _orderService = orderService;
+        _httpClient = httpClient;
+    }
+
+    [HttpPost("add")]
+    public async Task<IActionResult> CreateOrder([FromBody] Orders order)
+    {
+        if (order == null)
+        {
+            return BadRequest(new ApiResponse<string>("Error", null, "Invalid order data"));
+        }
+
+        var existProduct = await _orderService.CheckIfProductExistsAsync(order.ProductCode);
+
+        if (existProduct == false) {
+            return BadRequest(new ApiResponse<string>("Error", null, "The product code is incorrect"));
+        }
+
+        var existWorkTeam = await _orderService.CheckIfWorkTeamExistsAsync(order.TeamCode);
+
+        if (existWorkTeam == false)
+        {
+            return BadRequest(new ApiResponse<string>("Error", null, "The work team code is incorrect"));
+        }
+
+        order.OrderCode = await _orderService.GenerateNextOrderCodeAsync();
+
+        var createdOrder = await _orderService.CreateOrderAsync(order);
+
+        if (createdOrder == null)
+        {
+            return StatusCode(500, new ApiResponse<string>("Error", null, "Order was created but could not be retrieved"));
+        }
+
+        return CreatedAtAction(nameof(CreateOrder),
+            new { codice = createdOrder.OrderCode },
+            new ApiResponse<Orders>("success", createdOrder, "Order created successfully"));
+    }
+}
---
+++ Services/OrderServices.cs
@@ -0,0 +1,106 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using services_order_add.Database;
+using services_order_add.Models;
+using System.Net.Http;
+using NewtonsoftJson = Newtonsoft.Json.JsonConvert;
+
+namespace services_order_add.Services
+{
+    public class OrderServices
+    {
+        private readonly DBContext _context;
+        private readonly HttpClient _httpClient;
+
+        public OrderServices(DBContext context, HttpClient httpClient)
+        {
+            _context = context;
+            _httpClient = httpClient;
+        }
+
+        public async Task<Orders> CreateOrderAsync(Orders order)
+        {
+            order.Status = true;
+            order.CreatedAt = DateTime.Now;
+            order.UpdatedAt = DateTime.Now;
+
+            var createOrder = _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+            var idOrder = createOrder.Entity.Id;
+
+            var result =  await _context.Orders.FirstOrDefaultAsync(x => x.Id == idOrder);
+
+            return order;
+        }
+
+        public async Task<List<Orders>> GetAllOrdersAsync()
+        {
+            return await _context.Orders.Where(x => x.Status == true).ToListAsync();
+        }
+
+        public async Task<Orders> GetOrderByCodeAsync(string orderCode)
+        {
+            return await _context.Orders.FirstOrDefaultAsync(order => order.OrderCode == orderCode && order.Status == true);
+        }
+
+        public async Task<string> GenerateNextOrderCodeAsync()
+        {
+            var quantity = await _context.Orders.CountAsync();
+            var nextCode = $"ORD_{quantity + 1}";
+            return nextCode;
+        }
+
+        public async Task<bool> CheckIfProductExistsAsync(string codice)
+        {
+            //var url = $"http://localhost:3000/products/byCodiceProducto?codice={codice}"; // Local
+            var url = $"http://app_producto_search:3000/products/byCodiceProducto?codice={codice}";
+            try
+            {
+                var response = await _httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var apiResponse = NewtonsoftJson.DeserializeObject<ApiResponse<JArray>>(responseContent);
+                    if (apiResponse != null && apiResponse.Status == "success")
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> CheckIfWorkTeamExistsAsync(string codice)
+        {
+            //var url = $"http://localhost:8180/work-team/{codice}"; // Local
+            var url = $"http://app_work_team_search:8080/work-team/{codice}";
+            try
+            {
+                var response = await _httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var apiResponse = NewtonsoftJson.DeserializeObject<ApiResponse<JObject>>(responseContent);
+                    if (apiResponse != null && apiResponse.Status == "success")
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
+
